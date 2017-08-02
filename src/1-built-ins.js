c255ceea2bd44a96229d69ca955e48191e929299
Benjamin Huang <bhuang@hms.com>
2017-08-02 17:06:12 -0400

    add require

--- src/1-built-ins.js
+++ src/1-built-ins.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var one = require('./one');
 
 var toMb = function (f) {
     return(Math.round((f/1024/1024)*100/100));
@@ -7,3 +8,10 @@ var toMb = function (f) {
 console.log('Host: '+os.hostname());
 console.log('15 min. load average: '+os.loadavg()[2]);
 console.log(toMb(os.freemem())+' of '+toMb(os.totalmem())+' Mb free');
+
+one.doIt(23, function (err, result) {
+    console.log(result);
+});
+
+console.log(one.foo);
+console.log(one.count);
---
+++ src/one.js
@@ -0,0 +1,9 @@
+var count = 2;
+
+var doIt = function (i, callback) {
+    //do something, invoke callback
+}
+
+module.exports.doIt = doIt;
+module.exports.foo ='bar';
+module.exports.count = count;
