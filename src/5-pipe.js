f16b1a92739feb966dee3fa6c59b8ff771fee7df
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-08-02 22:05:07 -0400

    add createwritestream

--- src/5-pipe.js
+++ src/5-pipe.js
@@ -1,5 +1,9 @@
 var request = require('request');
+var fs = require('fs');
+
 
 var s = request("http://www.benjaminhuang.com");
 
 s.pipe(process.stdout);
+
+s.pipe(fs.createWriteStream('benjaminhuang.html'));
